2f71836daa8a0247be0f8a5c730cb962a04c9e50
Alex Devassy <31893005+alexdevassy@users.noreply.github.com>
2024-01-03 19:45:40 +0530

    Update aiexecuter.py

--- Dolos_ML_CTF_Challenge/aiexecuter.py
+++ Dolos_ML_CTF_Challenge/aiexecuter.py
@@ -2,14 +2,15 @@ import argparse
 from langchain_experimental.pal_chain import PALChain
 from langchain import OpenAI
 
-llm = OpenAI(temperature=0, openai_api_key="<api key>")
-pal_chain = PALChain.from_math_prompt(llm, verbose=True)
-
-parser = argparse.ArgumentParser(description="A simple script to accept user input via a flag.")
+parser = argparse.ArgumentParser(description="A simple script to accept user input via flags.")
 # Add a command line argument for user input
-parser.add_argument('--user_input', type=str, help='User input to be printed.')
+parser.add_argument('--user_input', type=str, help='prompt')
+parser.add_argument('--api_key', type=str, help='openai api key')
 # Parse the command line arguments
 args = parser.parse_args()
+openaiapikey = args.api_key
+llm = OpenAI(temperature=0, openai_api_key=openaiapikey)
+pal_chain = PALChain.from_math_prompt(llm, verbose=True)
 
 # Check if the 'user_input' flag is provided
 try:
